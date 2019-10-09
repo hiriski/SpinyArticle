1c738a76ddccf9cf3c7228d95044c7c714e0b8aa
riskiwebid <riski.web.id@zoho.com>
2019-10-09 10:03:55 +0700

    Add HTML5 Validasi and Remove Bootstrap

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!-- <link rel="stylesheet" type="text/css" href="assets/vendor/bootstrap/css/bootstrap.min.css"> -->
+  <title>SPINY GO</title>
+</head>
+<body>
+  
+<div id="spiny-go">
+  <main>
 
-<style type="text/css">
+
+	<style type="text/css">
 .divi { padding:0px 15px 15px 15px; font-family:calibri;}
 textarea, input {background:#f3f3f3; padding:5px; border:none;}
 	b{ font-size:20px; text-transform:uppercase;}
@@ -26,3 +40,10 @@ jumlah artikel yang ingin digenerate<br>
 
 </form>
 </div>
+
+  </main>
+</div>
+
+<script src="src/js/index.js"></script>
+</body>
+</html>
